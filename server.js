a2d1f4879db59eac9cf7394c5828c0e3c0ad62db
AkintolaDavid <akintoladvaid66@gmail.com>
2024-12-11 14:51:32 +0100

    commit

--- server.js
+++ server.js
@@ -14,8 +14,13 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
-app.use(cors());
-// Connect to MongoDB
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://techbams.vercel.app"], // Replace with your frontend URL in production
+  methods: "GET,POST,PUT,DELETE,PATCH",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+app.use(cors(corsOptions));
 connectDB(); // Using the `connectDB` function from db.js
 
 // Routes
