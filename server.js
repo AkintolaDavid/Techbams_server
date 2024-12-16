e1f6e2f36dccd560fc4414d3154782c5c9d4aa17
AkintolaDavid <akintoladvaid66@gmail.com>
2024-12-16 12:24:34 +0100

    commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 const corsOptions = {
   origin: ["http://localhost:5173", "https://techbams.vercel.app"], // Replace with your frontend URL in production
   methods: "GET,POST,PUT,DELETE,PATCH",
-  allowedHeaders: "Content-Type,Authorization",
+  allowedHeaders: "Content-Type,Authorization,multipart/form-data",
 };
 
 app.use(cors(corsOptions));
