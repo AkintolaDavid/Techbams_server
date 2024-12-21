56d14d3929a404e496ec96fe1deb1594e2aeb2c8
AkintolaDavid <akintoladvaid66@gmail.com>
2024-12-21 05:54:23 +0100

    commit

--- middleware/verifyAdminToken .js
+++ middleware/verifyAdminToken .js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const verifyAdminToken = (req, res, next) => {
+  console.log(req.headers.authorization);
   const token = req.headers.authorization?.split(" ")[1]; // Extract token from Bearer
 
   if (!token) {
