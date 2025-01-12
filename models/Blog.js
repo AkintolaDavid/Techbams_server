76250538126b24a9b4ffbc10f4699792b34fa862
AkintolaDavid <akintoladvaid66@gmail.com>
2025-01-12 18:12:35 +0100

    Initial commit

--- models/Blog.js
+++ models/Blog.js
@@ -4,6 +4,7 @@ const blogSchema = new mongoose.Schema({
   title: { type: String, unique: true },
   description: String,
   img: String,
+  dateCreated: { type: Date, default: Date.now },
 });
 
 const Blog = mongoose.model("Blog", blogSchema);
